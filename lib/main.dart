7d8712d2cad22dc91794eba18b7ed55b53c83d47
ocnslt <vinadr@student.ub.ac.id>
2026-06-11 12:33:42 +0700

    Fix: Membersihkan main.dart dan memperbarui file root

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-// 1. Import file dashboard Anda (pastikan namanya sesuai dengan file di folder lib Anda)
-import 'page_dashboard.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +9,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FILKOM Apps Redesign',
-      debugShowCheckedModeBanner: false, // Menghilangkan banner debug di pojok kanan
-      theme: ThemeData(
-        useMaterial3: true,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('FILKOM Apps Redesign Workspace'),
+        ),
       ),
-      // 2. Ganti text polos tadi dengan memanggil class dari page_dashboard.dart
-      home: const page_dashboard(), 
     );
   }
 }
